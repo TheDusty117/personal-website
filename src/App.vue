649cbc0669e29ec01bcdaec3699fc33034567edf
TheDusty117 <gabrieledipassio@gmail.com>
2023-06-29 17:53:29 +0200

    FEAT bg coloring in app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <script setup>
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
-import Footer from './components/Footer.vue';
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  
-  <Header/>
-  <Main/>
-  <Footer/>
-
+  <Header />
+  <Main />
+  <Footer />
 </template>
 
 <style lang="scss">
+@use "./style/general.scss" as *;
 
-@use './style/general.scss' as *;
+body {
+  // background-color: rgb(107, 107, 107);
+  background: linear-gradient(180deg, #ff9678 50%, #41436a 50%);
 
-body{
-  background-color: rgb(107, 107, 107);
   scroll-behavior: smooth;
 }
-
-
-
 </style>
